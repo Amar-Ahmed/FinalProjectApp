93e2c0dc1a681428cec0071f3e15fcd81fbc3e1c
Amar Ahmed <amar11432@gmail.com>
2018-06-05 21:25:51 -0400

    created retrofitInstance

--- app/src/main/java/android/com/androidappfinal/network/RetrofitClientInstance.java
+++ app/src/main/java/android/com/androidappfinal/network/RetrofitClientInstance.java
@@ -1,6 +1,7 @@
 package android.com.androidappfinal.network;
 
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by C4Q on 6/5/18.
@@ -8,7 +9,15 @@ import retrofit2.Retrofit;
 
 public class RetrofitClientInstance {
     private static Retrofit retrofit;
-    private static final String BASE_URL = "https://jsonplaceholder.typicode.com";
+    private static final String BASE_URL = "https://deckofcardsapi.com";
 
-
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            retrofit = new retrofit2.Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
 }
